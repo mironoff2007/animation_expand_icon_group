a7d2bdd61fd2db4fc8357afad97a5b84ea892751
vivotab8 <mironoff2007@rambler.ru>
2022-02-08 22:24:25 +0300

    Animate touch event

--- app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
@@ -1,7 +1,15 @@
 package com.mironov.animationexpandiconsgroup
 
 import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.AnimationUtils
 import android.widget.TableLayout
+import androidx.core.view.children
 
-class ExpandableLayout(context: Context?) : TableLayout(context) {
+class ExpandableLayout(context: Context?, attrs: AttributeSet?) : TableLayout(context, attrs) {
+
+    fun touch(){
+        this.startAnimation(AnimationUtils.loadAnimation(this.context,R.anim.touch_impact_animation))
+    }
+
 }
--- app/src/main/java/com/mironov/animationexpandiconsgroup/MainActivity.kt
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/MainActivity.kt
@@ -2,10 +2,26 @@ package com.mironov.animationexpandiconsgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mironov.animationexpandiconsgroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.container1.setOnClickListener { binding.container1.touch() }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
