c76689a0e8a0d0682a638e6a5d24d35ff713071d
vivotab8 <mironoff2007@rambler.ru>
2022-02-08 19:16:44 +0300

    Table of icons is added

---
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
@@ -0,0 +1,7 @@
+package com.mironov.animationexpandiconsgroup
+
+import android.content.Context
+import android.widget.TableLayout
+
+class ExpandableLayout(context: Context?) : TableLayout(context) {
+}
