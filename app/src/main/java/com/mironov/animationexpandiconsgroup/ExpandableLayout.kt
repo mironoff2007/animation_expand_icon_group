bdcdad35a986f6d2343933f5c1248a2545eed24d
Mironov Ury <mironoff2007@rambler.ru>
2022-02-09 00:56:45 +0300

    Expand and shrink container with icons

--- app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
@@ -1,16 +1,36 @@
 package com.mironov.animationexpandiconsgroup
 
-import android.animation.Animator
+
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TableLayout
-import androidx.core.view.children
+import kotlin.math.roundToInt
 
-class ExpandableLayout(context: Context?, attrs: AttributeSet?) : TableLayout(context, attrs) {
+
+class ExpandableLayout : TableLayout {
 
     private var expanded=false
+    private var heightIncrement=0
+    private val expandAndShrinkDuration=200L
+
+    @SuppressLint("Recycle")
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        var expandBy=0
+        if (attrs != null) {
+            val a = context!!.obtainStyledAttributes(attrs, R.styleable.ExpandableLayout)
+            if (a.hasValue(R.styleable.ExpandableLayout_expandBy)) {
+                expandBy = a.getInt(R.styleable.ExpandableLayout_expandBy,0)
+            }
+        }
+        heightIncrement= dpToPx(expandBy).roundToInt()
+    }
+
 
     fun touch(){
         if(!expanded){
@@ -33,12 +53,38 @@ class ExpandableLayout(context: Context?, attrs: AttributeSet?) : TableLayout(co
         }
     }
 
-    fun expand(){
+    private fun expand(){
         expanded=true
+
+        animateHeightResize(heightIncrement)
     }
 
-    fun shrink(){
+    private fun shrink(){
         expanded=false
+        animateHeightResize(-heightIncrement)
+    }
+
+    private fun animateHeightResize(increment:Int){
+        val valueAnimator =
+            ValueAnimator.ofInt(this.measuredHeight, this.measuredHeight +increment)
+        valueAnimator.duration = expandAndShrinkDuration
+        valueAnimator.addUpdateListener {
+            val animatedValue = valueAnimator.animatedValue as Int
+            val layoutParams = this.layoutParams
+            layoutParams.height = animatedValue
+            this.layoutParams = layoutParams
+            this.invalidate()
+        }
+        valueAnimator.start()
+    }
+
+    fun dpToPx(dp: Int): Float {
+        val r: Resources = resources
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            r.displayMetrics
+        )
     }
 
 }
