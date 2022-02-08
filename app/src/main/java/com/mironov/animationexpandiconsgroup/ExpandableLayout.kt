b9a72200d6a8666f07b37ec43caf3bf182c663bf
vivotab8 <mironoff2007@rambler.ru>
2022-02-08 23:12:59 +0300

    Switcher is added for expand shrink methods

--- app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/ExpandableLayout.kt
@@ -1,15 +1,44 @@
 package com.mironov.animationexpandiconsgroup
 
+import android.animation.Animator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TableLayout
 import androidx.core.view.children
 
 class ExpandableLayout(context: Context?, attrs: AttributeSet?) : TableLayout(context, attrs) {
 
+    private var expanded=false
+
     fun touch(){
-        this.startAnimation(AnimationUtils.loadAnimation(this.context,R.anim.touch_impact_animation))
+        if(!expanded){
+        val touchAnimation=AnimationUtils.loadAnimation(this.context,R.anim.touch_impact_animation)
+        touchAnimation.setAnimationListener(
+            object :  Animation.AnimationListener {
+
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    expand()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+
+            })
+        this.startAnimation(touchAnimation)}
+        else{
+            shrink()
+        }
+    }
+
+    fun expand(){
+        expanded=true
+    }
+
+    fun shrink(){
+        expanded=false
     }
 
 }
