6f6b4c891874a9a204ea21a8ff1f44dd8c8a6511
Mironov Ury <mironoff2007@rambler.ru>
2022-02-09 13:09:48 +0300

    Fix onBack pressed view fade multiple times

--- app/src/main/java/com/mironov/animationexpandiconsgroup/MainActivity.kt
+++ app/src/main/java/com/mironov/animationexpandiconsgroup/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mironov.animationexpandiconsgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mironov.animationexpandiconsgroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +11,64 @@ class MainActivity : AppCompatActivity() {
 
     private val binding get() = _binding!!
 
+    private lateinit var array: Array<ExpandableLayout>
+
+    private var expandedView: ExpandableLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.container1.setOnClickListener { binding.container1.touch() }
+        array =
+            arrayOf(binding.container1, binding.container2, binding.container3, binding.container4)
+
+        binding.container1.setOnClickListener {
+            expand(it as ExpandableLayout)
+        }
+
+        binding.container2.setOnClickListener {
+            expand(it as ExpandableLayout)
+        }
+
+        binding.container3.setOnClickListener {
+            expand(it as ExpandableLayout)
+        }
+
+        binding.container4.setOnClickListener {
+            expand(it as ExpandableLayout)
+        }
+    }
+
+    fun expand(it: ExpandableLayout) {
+        expandedView = it
+        it.touch()
+        fadeOthers(it)
+    }
+
+    private fun fadeOthers(view: View) {
+        array.forEach {
+            if (view != it) {
+                it.fade()
+            }
+        }
+    }
+
+    private fun showOthers(view: View) {
+        array.forEach {
+            if (view != it) {
+                it.show()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (expandedView != null) {
+            expandedView!!.shrink()
+            showOthers(expandedView!!)
+            expandedView = null
+        }
 
     }
 
